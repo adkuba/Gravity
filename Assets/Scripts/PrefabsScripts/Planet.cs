516efbde1d80fa22ba1da2e3087dd5674e00d00c
Jakub Adamski <kuba@exemplum.pl>
2021-07-01 15:10:54 +0200

    planets-asset

--- Assets/Scripts/PrefabsScripts/Planet.cs
+++ Assets/Scripts/PrefabsScripts/Planet.cs
@@ -8,11 +8,8 @@ public class Planet : MonoBehaviour
     private Vector2 widthBounds;
     private Vector2 heightBounds;
     public bool fuel;
-    private ParticleSystem sparks;
     private GameObject moon;
     private GameObject player;
-    public Material[] materials;
-    private float score;
 
 
     void Start()
@@ -31,77 +28,20 @@ public class Planet : MonoBehaviour
         
         //additional effects
         rand = Random.value;
-        bool sparksK = false;
         bool moonK = false;
-        bool find = false;
-        if (rand <= .4f && !find)
+        if (rand <= .4f)
         {
             moonK = true;
-            find = true;
         }
-        if (rand <= .8f && !find)
-        {
-            sparksK = true;
-            find = true;
-        }
-        if (rand <= .9 && !find)
-        {
-            sparksK = true;
-            moonK = true;
-        }
-
-        //sparkles size and colour
-        sparks = transform.GetChild(0).GetComponent<ParticleSystem>();
-        float sSize = transform.localScale.x * 0.14f - 0.2f;
-        sparks.transform.localScale = new Vector3(sSize, sSize, sSize);
-        if (player.GetComponent<PlayerController>() != null)
-        {
-            score = player.GetComponent<PlayerController>().getScore();
-        }
-        else if (player.GetComponent<MenuPlayer>() != null)
-        {
-            score = player.GetComponent<MenuPlayer>().getScore();
-        }
-        else
-        {
-            score = player.GetComponent<PlayerTutorial>().getScore();
-        }
-        int index = 0;
-        if (score < 400)
-        {
-            index = 0;
-        }
-        else if (score < 800)
-        {
-            index = 1;
-        }
-        else if (score < 1200)
-        {
-            index = 2;
-        }
-        else if (score < 1600)
-        {
-            index = 3;
-
-        }
-        else
-        {
-            index = 4;
-        }
-        sparks.GetComponent<ParticleSystemRenderer>().trailMaterial = materials[index];
 
         //moon size
         float moonSize = Random.Range(0.2f, 0.4f);
-        moon = transform.GetChild(2).gameObject;
+        moon = transform.GetChild(0).gameObject;
         moon.transform.localScale = new Vector3(moonSize, moonSize, moonSize);
 
         if (!moonK)
         {
-           Destroy(transform.GetChild(2).gameObject);
-        }
-        if (!sparksK)
-        {
-            Destroy(transform.GetChild(0).gameObject);
+           Destroy(transform.GetChild(0).gameObject);
         }
     }
 
